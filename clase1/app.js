04782f0d082377c28cc339414cc0a8eb087d39d1
Santiago <szapatamejia193@gmail.com>
2023-09-05 11:54:18 -0500

    clase programacion

---
+++ clase1/app.js
@@ -0,0 +1,34 @@
+console.log("Santiago papasito")
+
+const TOKEN = "1234567890";
+let counter = 10;
+console.log("valor de counter= ", counter);
+counter = "veinte";
+console.log("valor de counter = ", counter);
+
+let a = 10;
+let b = 20;
+let c = 30;
+let d = a + b + c;
+let e = a * b;
+let f = a / b;
+let g = a - b;
+let h = a % b;
+console.log("console.log -> ", d);
+console.log("console.log -> ", e);
+console.log("console.log -> ", f);
+console.log("console.log -> ", g);
+console.log("console.log -> ", h);
+
+//operadores logicos
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a && b);
+console.log(a || b);
+
+let condicion = true;
+if (condicion) {
+    console.log("console.log")
+} 
---
+++ clase1/indexciaf.js
@@ -0,0 +1,24 @@
+
+//este es un comentario
+
+
+/*
+Habla pa ve
+*/
+
+console.log("Hello Ciaf")
+
+let myname="Santiaguito";
+console.log(myname) 
+console.log(
+    `mi nombre es: ${myname} y soy de pereira, Mi apellido es ${mylastname}. Mi edad es ${age}`
+)
+
+
+let age="17"
+if (age <18)
+{console.log("Si hay carcel")}
+    
+else (age >=18)
+{console.log("No hay carcel")}
+
