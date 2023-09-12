ea4602f4d17312d428dc7578c570ef3a9a6792c5
Santiago <szapatamejia193@gmail.com>
2023-09-11 21:20:03 -0500

    Changes Clases

---
+++ clase1/index.js
@@ -0,0 +1,72 @@
+console.log("2023-09-11 - programacion III")
+let numberOne = 10;
+let result = 10 % 4; // Opreacion residuo (%)
+console.log("result:", result); 
+
+
+
+console.log("2023-09-11 - programacion III")
+let numberTwo = 10;
+let result1 = 10**2; // Opreacion Exponente (**)
+console.log("result:", result); 
+
+
+let brand = "Mazda"
+console.log(brand !="mazda") //Operacion (Signos de comparacion)
+
+console.log("5"==5)
+console.log("5"===5)
+console.log("5"==5)
+console.log("5"!=5)
+
+let num1 = 10;
+let num2 = 9; 
+console.log(num1>=num2) 
+
+let num3 = 0;
+let num4 = 0;
+console.log(num3==num4) 
+
+console.log(10>=9)
+console.log(0==0)
+
+
+"midudev" .length //7
+"midudev" [1] //i
+"midudev" .includes("dev") //true
+"midudev" .indexOf("midu") //0
+"midudev" .startsWith // true
+"midudev" .endsWith // false
+"midudev" .slice(0,4) // "midu"
+"midudev" .slice(4) // dev
+"midudev" .toUpperCase // MIDUDEV
+"midudev" .toLowerCase // midudev
+"midudev" .replace ("dev" , "☣")// "midu☣"
+"midudev" .repeat(3) //"midumidumidu"
+" mi du " .trim() // "mi du"
+"mi du dev" .split("") // ["mi","du","dev"]
+
+
+console.log(2 < 3 && 3 < 4 )
+
+// OPERADORES DE CADENA.CONCATENACION
+
+console.log("Operador de concatenacion")
+
+let first_name = "Santiago"
+let last_name = "Zapata"
+let complete_name = first_name.toUpperCase() + " " + last_name.toUpperCase()
+
+console.log(complete_name) //Santiago Zapata
+
+let age = 20 
+if (age >=18 ){
+    console.log("Es mayor de edad")
+}else{
+    console.log("Es carcel mi papa")
+}
+
+
+let counter = 7
+counter > 10 ? console.log("counter es mayor a 10"):console.log("counter es menor a 10")
+
