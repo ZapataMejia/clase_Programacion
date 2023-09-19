6cdfed6907210bef10794d0c117a1f7d8f63c22a
Santiago <Szapatamejia193@gmail.com>
2023-09-18 21:11:09 -0500

    Changes Class 2

--- clase2/ejercicio1.js
+++ clase2/ejercicio1.js
@@ -1,20 +1,27 @@
 
 // Ejercicio 1
-
+/*
 let numero =8
 if (numero%2==0){
     console.log("Es par")
 } else{
     console.log("Es impar")
 }
+*/
 
 // Ejercicio 2 
-
-const numero1 = 17
-if (numero1 >= 18) {
-    console.log("Eres mayor de numero1")
-} else if (numero1 >= 16) {
-    console.log("Eres casi mayor de numero1")
+/*
+const numero = -4
+if (numero == 0) {
+    console.log("Numero es Cero")
+} else if (numero1 > 0) {
+    console.log("Es positivo")
 } else {
-    console.log("Eres menor de numero1")
+    console.log("Es negativo")
 }
+*/
+
+// Ejercicio 3 
+
+
+
